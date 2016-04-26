3c9378769cfae63b4ab12c2d63e352c6dd0c3e7f
Solomiia <solomiya.kachan@gmail.com>
2016-04-26 23:59:00 +0300

    Done level 1

---
+++ lesson2-1/task1.py
@@ -0,0 +1,16 @@
+a = 32
+b = 37.2
+c = -102.345
+d = 45
+e = 11
+f = 3
+g = 0.002
+h = 11
+
+part1 = (1 + 2 * a) / 3
+part2 = 4 * (b + c) * (5 - d - e) / f
+part3 = 6 * (7 / g + h) ** 2
+
+result = part1 + part2 - part3
+
+print(result)
---
+++ lesson2-1/task3.py
@@ -0,0 +1,8 @@
+tuple_test = (1, 2, 2, 3, 4, 1, 5)
+list_test = [1, -1, 'a', '1', 2, None, True, 'a']
+
+unique_el1 = set(tuple_test)
+unique_el2 = set(list_test)
+
+print(unique_el1)
+print(unique_el2)
