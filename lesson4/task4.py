45026d15ef397ed59cf78bb37faa23f105cd783d
Solomiia <solomiya.kachan@gmail.com>
2016-06-06 00:13:39 +0300

    Add task4 to lesson4

---
+++ lesson4/task4.py
@@ -0,0 +1,13 @@
+﻿from lxml import etree
+
+def main():
+    course_xml = etree.parse('course.xml')
+    xslt = etree.parse('course_transform.xml')
+
+    transform = etree.XSLT(xslt)
+    result = transform(course_xml)
+
+    print(etree.tostring(result, pretty_print=True))
+
+if __name__ == '__main__':
+    main()
