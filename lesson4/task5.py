9ee8227c1f8579bb0845d8fa5008b80c90074555
Solomiia <solomiya.kachan@gmail.com>
2016-06-06 23:17:26 +0300

    Added task5 to lesson4

---
+++ lesson4/task5.py
@@ -0,0 +1,31 @@
+﻿
+import json
+import requests
+
+from requests.auth import HTTPBasicAuth
+
+ENDPOINT = 'https://python-for-qa.herokuapp.com/login'
+
+def read_file(file):
+    with open(file, 'r') as f:
+        file_str = f.read()
+        return file_str
+
+def login(usr, psw):
+    response = requests.get(ENDPOINT, auth = HTTPBasicAuth(usr, psw))
+    if response.status_code == 401:
+        print("Client:'" + usr +"' with password:'" + psw + "' - don't have access to web service.")
+
+
+def main():
+    str_json = read_file('clients.json')
+    json_data = json.loads(str_json)
+    
+    for item in json_data:
+        user_name = item['name']
+        password = item['password']
+        login(user_name, password)
+
+
+if __name__ == '__main__':
+    main()
