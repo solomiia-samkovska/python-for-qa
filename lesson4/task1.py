3c88e6af91a864bf559d26e6854f24bbe5f92fb3
Solomiia <solomiya.kachan@gmail.com>
2016-06-07 00:09:19 +0300

    Fixed task1 from lesson4

--- lesson4/task1.py
+++ lesson4/task1.py
@@ -1,11 +1,16 @@
+# -*- coding: utf-8 -*-
 from lxml import html
 
 
 def main():
-    dom = html.parse('http://deshevshe.net.ua/multifunctiondevice/')
+    dom = html.parse('http://deshevshe.net.ua/laptop-asus/asus_x540lj_x540lj_xx002d_chocolate_blac')
 
-    price = dom.find('//*[@id="viewGaleryBl"]/div[1]/div/div[2]/div[1]')
-    print(price.text)
+    price = dom.find('//*[@itemprop="price"]')
+    print(u"Price: " + price.text)
+
+    exist = dom.find('//*[@class="wareAvail"]')
+    if exist.text_content() == u'Нет в наличии':
+        print("The product is out of stock")
 
 if __name__ == '__main__':
     main()
